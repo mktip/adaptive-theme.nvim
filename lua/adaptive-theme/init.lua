9fc085640d0e881ece1f72814c82d91951823d92
Mohammad KT. Issa <mo.issa.ok@gmail.com>
2024-06-27 03:47:23 +0300

    remove tabs

--- lua/adaptive-theme/init.lua
+++ lua/adaptive-theme/init.lua
@@ -1,26 +1,26 @@
 local M = {}
 
 local default_theme_handler = function(background)
-	if background == "none" then
-		return
-	end
-	vim.o.background = background
+  if background == "none" then
+    return
+  end
+  vim.o.background = background
 end
 
 
 local function theme_callback(theme_handler)
-	local uv = vim.loop
+  local uv = vim.loop
 
-	return uv.new_async(vim.schedule_wrap(function(background)
-		theme_handler(background)
-	end))
+  return uv.new_async(vim.schedule_wrap(function(background)
+    theme_handler(background)
+  end))
 end
 
 local function adaptive_theme_watcher(callback)
-	local ldbus = require("ldbus")
-	local uv = vim.loop
+  local ldbus = require("ldbus")
+  local uv = vim.loop
 
-	local AdaptiveColors = { LIGHT = "light", DARK = "dark", NONE = "none" }
+  local AdaptiveColors = { LIGHT = "light", DARK = "dark", NONE = "none" }
 
   local function read_initial_theme()
     local conn = assert ( ldbus.bus.get ( "session" ) )
@@ -45,71 +45,71 @@ local function adaptive_theme_watcher(callback)
 
   end
 
-	local function parse_msg(msg)
-		local iter = ldbus.message.iter.new()
+  local function parse_msg(msg)
+    local iter = ldbus.message.iter.new()
 
-		assert(msg:iter_init(iter), "Message has no parameters")
+    assert(msg:iter_init(iter), "Message has no parameters")
 
-		local result = AdaptiveColors.NONE
+    local result = AdaptiveColors.NONE
 
-		for char in msg:get_signature():gmatch(".") do
-			if char == "v" then
-				local sub_iter = iter:recurse()
-				local sigvalue = sub_iter:get_basic()
+    for char in msg:get_signature():gmatch(".") do
+      if char == "v" then
+        local sub_iter = iter:recurse()
+        local sigvalue = sub_iter:get_basic()
 
-				assert(
-					sub_iter:get_arg_type() == ldbus.types.uint32,
-					"Expected adaptive theme state to be uint32, got " .. sub_iter:get_arg_type()
-				)
+        assert(
+          sub_iter:get_arg_type() == ldbus.types.uint32,
+          "Expected adaptive theme state to be uint32, got " .. sub_iter:get_arg_type()
+        )
 
-				if sigvalue == 1 then -- 0 = light, 1 = dark
-					result = AdaptiveColors.DARK
-				else
-					result = AdaptiveColors.LIGHT
-				end
-			end
+        if sigvalue == 1 then -- 0 = light, 1 = dark
+          result = AdaptiveColors.DARK
+        else
+          result = AdaptiveColors.LIGHT
+        end
+      end
 
-			if not iter:next() then
-				break
-			end
-		end
+      if not iter:next() then
+        break
+      end
+    end
 
-		return result
-	end
+    return result
+  end
 
 
-	local conn = assert(ldbus.bus.get("session"))
+  local conn = assert(ldbus.bus.get("session"))
 
   uv.async_send(callback, callback(read_initial_theme()))
 
-	assert(ldbus.bus.add_match(
-		conn,
-		table.concat({
-			"type='signal'",
-			"interface='org.freedesktop.portal.Settings'",
-			"member='SettingChanged'",
-			"arg0='org.freedesktop.appearance'",
-			"arg1='color-scheme'",
-		}, ",")
-	))
-	conn:flush()
+  assert(ldbus.bus.add_match(
+    conn,
+    table.concat({
+      "type='signal'",
+      "interface='org.freedesktop.portal.Settings'",
+      "member='SettingChanged'",
+      "arg0='org.freedesktop.appearance'",
+      "arg1='color-scheme'",
+    }, ",")
+  ))
+  conn:flush()
 
-	while conn:read_write() do
-		local msg = conn:pop_message()
-		if msg:get_type() == "signal" then
-			uv.async_send(callback, parse_msg(msg))
-		end
-	end
+  while conn:read_write() do
+    local msg = conn:pop_message()
+    if msg:get_type() == "signal" then
+      uv.async_send(callback, parse_msg(msg))
+    end
+  end
 end
 
 function M.setup(options)
-	if not options then
-		options = {}
-	end
+  if not options then
+    options = {}
+  end
 
-	local uv = vim.loop
+  local uv = vim.loop
 
-	M.theme_handler = options.theme_handler or default_theme_handler
+  M.theme_handler = options.theme_handler or default_theme_handler
 
   -- Query current theme and run the theme handler initially
   local ldbus = require("ldbus")
@@ -130,8 +130,8 @@ function M.setup(options)
 
   print(msg)
 
-	-- Start the watcher thread
-	local watcher_thread = uv.new_thread(adaptive_theme_watcher, theme_callback(M.theme_handler))
+  -- Start the watcher thread
+  local watcher_thread = uv.new_thread(adaptive_theme_watcher, theme_callback(M.theme_handler))
 end
 
 
